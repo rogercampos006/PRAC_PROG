7ae053e8d6c821c2cbc69f5644a5a01304ed2758
bielpj <1745043@uab.cat>
2025-04-16 12:12:00 +0200

    Update CODI.py

--- CODI.py
+++ CODI.py
@@ -1,1 +1,18 @@
+import csv
+import numpy as np
+
+class Recomana:
+
+class Simple(Recomana):
+
+class Colaboratiu(Recomana):
+
+class Dataset:
+    def __init__(self, arxiu):
+        self.dades = np.matrix()
+        self.arxiu = arxiu
+
+    def import_data(self):
+        #Llegir Dades i Afegir a la matriu
+
 
